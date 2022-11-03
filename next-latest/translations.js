b3573c6e2423493bb5bdff64ebcbed04a213e977
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-11-03 18:23:14 +0400

    added lang translations with i18n

---
+++ next-latest/translations.js
@@ -0,0 +1,19 @@
+export const enUS = {
+    T01: "Welcome to",
+    T02: "Documentation",
+    T03: "Find in-depth information about Next.js features and API",
+    T04: "Learn",
+    T05: "Learn about Next.js in an interactive course with quizzes!",
+    T06: "Examples",
+    T07: "Discover and deploy boilerplate example Next.js projects"
+}
+
+export const Ka ={
+    T01: "მოგესალმებით",
+    T02: "დოკუმენტაცია",
+    T03: "გაიგე მეტი Next.js მახასიათებლებსა და API-ს შესახებ",
+    T04: "ისწავლე",
+    T05: "ისწავლე Next.js ინტერაქტიული კურსით და ამოცანებით",
+    T06: "მაგალითები",
+    T07: "აღმოაჩინე და განათავსე Next.js-ის პროექტები"
+}
